11abd92e40e3e5affb97c92e325b617554456e80
ncls-brn <nicolas.j.baran@gmail.com>
2022-07-28 12:06:08 +0200

    maj

--- js/dataviz.js
+++ js/dataviz.js
@@ -35,6 +35,7 @@ let marker = L.marker([48.856614, 2.3522219], { icon: issIcon });
 if (document.documentElement.clientWidth < 577) {
   // console.log("resize <577")
   map.setView([0, 0], 0);
+  map.setMinZoom(0);
   // console.log(map.getZoom());
 } else if (
   (document.documentElement.clientWidth < 1201) &
@@ -42,6 +43,7 @@ if (document.documentElement.clientWidth < 577) {
 ) {
   // console.log("resize 576<1200")
   map.setView([0, 0], 1);
+  map.setMinZoom(1);
   // console.log(map.getZoom());
 } else if (
   (document.documentElement.clientWidth < 1401) &
@@ -49,10 +51,12 @@ if (document.documentElement.clientWidth < 577) {
 ) {
   // console.log("resize 576<1200")
   map.setView([0, 0], 1.3);
+  map.setMinZoom(1.3);
   // console.log(map.getZoom());
 } else if (document.documentElement.clientWidth > 1400) {
   // console.log("resize >1200")
   map.setView([0, 0], 1.6);
+  map.setMinZoom(1.6);
   // console.log(map.getZoom());
 }
 
@@ -65,22 +69,31 @@ window.addEventListener("resize", function (event) {
   if (width < 577) {
     // console.log("resize <577")
     map.setView([0, 0], 0);
+    map.setMinZoom(0);
     // console.log(map.getZoom());
   } else if ((width < 1201) & (width > 576)) {
     // console.log("resize 576<1200")
     map.setView([0, 0], 1);
+    map.setMinZoom(1);
     // console.log(map.getZoom());
   } else if ((width < 1401) & (width > 1200)) {
     // console.log("resize 576<1200")
     map.setView([0, 0], 1.3);
+    map.setMinZoom(1.3);
     // console.log(map.getZoom());
   } else if (width > 1200) {
     //console.log("resize >1200")
     map.setView([0, 0], 1.6);
+    map.setMinZoom(1.6);
     //console.log(map.getZoom());
   }
 });
 
+//special html display for ocean and country 
+const ul = document.querySelector('#ul');
+let el = document.createElement("li");
+
+//call api ISS and geoapify and unsplash
 async function callIss() {
   let response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
   if (response.ok) {
@@ -90,23 +103,53 @@ async function callIss() {
     let json = await response.json();
     let lat = json.latitude;
     let lon = json.longitude;
+
+    //API Geoapify => 3000 requests/day free. For reverse geocoding to get the country of our iss lat/lon
+    let geocode = await fetch("https://api.geoapify.com/v1/geocode/reverse?lat="+lat+"&lon="+lon+"&apiKey=6e906858295b4b2e820808e2e670d511")
+
+    let geoJson = await geocode.json();
+    let country = geoJson.features[0].properties.country;
+    let ocean = geoJson.features[0].properties.ocean;
+
+    //geolocalize iss with lat and long and the icon
+    marker = L.marker([lat, lon], { icon: issIcon });
+
+    // //unsplash
+    // let unsplashKey ="_sf2avvjOlZgOtnH9ZBAjEpVp85tORwMPe5oG0WOBmw";
+    // let unsplashSecretKey ="RcxoK3BfW4hjZHlVKUZZJ2fHW6Q8LY1B5evwPM4H72I";
+
+    //html display with iss data
     document.getElementById("latitude").textContent =
       "Latitude : " + json.latitude;
     document.getElementById("longitude").textContent =
       "Longitude : " + json.longitude;
     document.getElementById("altitude").textContent =
-      "Altitude : " + json.altitude;
+      "Altitude : " + json.altitude + " kilometers";
     document.getElementById("speed").textContent =
-      "Velocity : " + json.velocity;
+      "Velocity : " + json.velocity + " km/h";
     document.getElementById("visibility").textContent =
       "Visibilty : " + json.visibility;
     document.getElementById("timing").textContent =
       "Timestamp : " + json.timestamp;
 
-    // geolocalize iss with lat and long and the icon
-    marker = L.marker([lat, lon], { icon: issIcon });
-    map.addLayer(marker);
+    if (country !== undefined){
+      //find a random photo with unsplash
+      // let unsplashUrl = "https://api.unsplash.com/photos/?client_id="+unsplashKey+"?client_secret="+unsplashSecretKey;
+      // let unsplashImgCountry = await fetch (unsplashUrl);
+      // console.log(unsplashImgCountry);
 
+      //html
+      el.innerHTML = "Country : " + country;
+      ul.appendChild(el);
+      marker.bindPopup("<b>This is the ISS !</b><br>It's now in <b>"+country+"</b> !").openPopup();
+      map.addLayer(marker);
+    } else if (ocean != undefined){
+        el.innerHTML = "Ocean : " + ocean;
+        ul.appendChild(el);
+        marker.bindPopup("<b>This is the ISS !</b><br>It's now in <b>"+ocean+"</b>!").openPopup();
+        map.addLayer(marker);
+    }
+    
     // compare iss's lon with map's lon to avoid the red line back
     if ((lon < 180) & (lon >= 0)) {
       //console.log("dans les plus");
@@ -131,7 +174,6 @@ callIss();
 let req = new XMLHttpRequest();
 let url = "https://api.nasa.gov/planetary/apod?api_key=";
 let api_key = "5B6oJsSCQyekXZvNOKpsUhRPl1e7FHqjIAyHpybk";
-
 req.open("GET", url + api_key);
 req.send();
 
